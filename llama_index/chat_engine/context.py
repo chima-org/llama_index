f86abdd3a7567c31bdd49ac4880238efd42a3909
Anchit Bhatia <anchitbhatia23@gmail.com>
2023-11-04 04:44:30 +0000

    Added method to get the augmented prompt

--- llama_index/chat_engine/context.py
+++ llama_index/chat_engine/context.py
@@ -252,6 +252,10 @@ class ContextChatEngine(BaseChatEngine):
             ],
             source_nodes=nodes,
         )
+        
+    async def get_augmented_prompt(self, message: str):
+        context_str_template, nodes = await self._agenerate_context(message)
+        return self._get_prefix_messages_with_context(context_str_template)
 
     @trace_method("chat")
     async def astream_chat(
